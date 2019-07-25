f5ff56a4f44dbda10a13866db5afb5de23c9e18d
Aditi Ohri <didiohri@gmail.com>
2019-07-25 22:28:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,3 +8,10 @@ array.push(`I am ${i} strange loops.`);
   }
   return array;
 }
+
+function whileLoop(number) {
+  let countdown = number;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+}
