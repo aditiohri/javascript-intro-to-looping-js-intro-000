6414a8ef925b52e03b661fa1a62c4351cf1f6f3e
Aditi Ohri <didiohri@gmail.com>
2019-07-25 22:20:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 forLoop(array) {
   for (let i=0; i<25; i++) {
     if (i===1) {
-return `array`, `I am 1 strange loop.`;
+array.push(`I am 1 strange loop.`);
 } else {
-return `array`, `I am ${i} strange loops.`;
+array.push(`I am ${i} strange loops.`);
   }
   }
   return array;
