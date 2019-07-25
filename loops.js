0ef72d96fea7ddf94b77f5f13960476c6721e589
Aditi Ohri <didiohri@gmail.com>
2019-07-25 22:42:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,14 @@ function whileLoop(n) {
   }
   return "done";
 }
+
+function doWhileLoop(num) {
+  
+function incrementVariable() {
+  num = num + 1;
+  return num;
+}
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
