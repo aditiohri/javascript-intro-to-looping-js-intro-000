78f589f0b863ce9a39576ddcc457a98aa66c392a
Aditi Ohri <didiohri@gmail.com>
2019-07-25 21:58:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 forLoop(array){
   for (let i=0; i<25; i++) {
     if (i=1) {
-  console.log("I am 1 strange loop.")  }
+  console.log("array ", "I am 1 strange loop.")  }
   else {
-    console.log("I am ${i} strange loops.)"
+    console.log("array ", "I am ${i} strange loops.)"
   }
   }
   return array;
